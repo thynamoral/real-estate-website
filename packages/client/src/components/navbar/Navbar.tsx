c6396f1b02eb0fce20d2ef2cc9e21ca702bbf625
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-06-13 11:13:49 +0700

    implement responsive navbar

--- packages/client/src/components/navbar/Navbar.tsx
+++ packages/client/src/components/navbar/Navbar.tsx
@@ -1,21 +1,43 @@
+import { Menu } from "lucide-react";
 import "./Navbar.scss";
+import useToggle from "@/hooks/useToggle";
 const Navbar = () => {
+  const { isToggle, setIsToggle } = useToggle();
+
+  console.log(isToggle);
+
   return (
     <nav>
       <div className="left">
         <a id="logo" href="/">
           MomoEstate
         </a>
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">Agent</a>
+        <div className="navLinks">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agent</a>
+        </div>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
+        <a id="signIn" href="/">
+          Sign in
+        </a>
         <a id="signUp" href="/">
           Sign up
         </a>
+        {/* nav links menu for mobile/tablet */}
+        <div id="menuIconContainer">
+          <Menu id="menuIcon" onClick={() => setIsToggle((prev) => !prev)} />
+        </div>
+        <div className={`${isToggle ? "menuList showMenu" : "menuList"}`}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agent</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
