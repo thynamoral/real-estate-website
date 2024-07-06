e6a73d89220b6a62d46c07710d00ba00da56896e
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-07-06 22:13:13 +0700

    implement Card component

---
+++ packages/client/src/components/card/Card.tsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import "./Card.scss";
+import TCard from "@/entities/Card";
+import {
+  Bath,
+  BedDouble,
+  Bookmark,
+  MapPin,
+  MessageSquareMore,
+} from "lucide-react";
+
+type Props = {
+  item: TCard;
+};
+
+const Card = ({ item }: Props) => {
+  return (
+    <div className="card">
+      <Link to={`${item.id}`} className="imgContainer">
+        <img src={item.img} alt={item.title} />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <MapPin className="icons" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="bedroom">
+              <BedDouble className="icons" />
+              <span>{item.bathroom} bedroom</span>
+            </div>
+            <div className="bathroom">
+              <Bath className="icons" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <button className="icon">
+              <Bookmark className="icons" />
+            </button>
+            <button className="icon">
+              <MessageSquareMore className="icons" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ packages/client/src/entities/Card.ts
@@ -0,0 +1,13 @@
+type TCard = {
+  id: number;
+  title: string;
+  img: string;
+  bedroom: number;
+  bathroom: number;
+  price: number;
+  address: string;
+  latitude: number;
+  longitude: number;
+};
+
+export default TCard;
