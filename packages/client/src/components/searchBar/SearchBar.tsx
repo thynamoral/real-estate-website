1083d4654184a00dbecfcb71dcfc183c0f217100
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-07-06 16:41:41 +0700

    implement SearchBar and responsive homePage

--- packages/client/src/components/searchBar/SearchBar.tsx
+++ packages/client/src/components/searchBar/SearchBar.tsx
@@ -1,7 +1,54 @@
+import { Search } from "lucide-react";
 import "./SearchBar.scss";
+import { useState } from "react";
+
+const types = ["buy", "rent"];
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (type: string) => setQuery((prev) => ({ ...prev, type }));
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            className={type === query.type ? "active" : ""}
+            onClick={() => switchType(type)}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form className="form">
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={1000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={1000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <Search id="searchBtn" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
